d23bbfb23c5b51ebd1e24e92eb0201d061fa63cb
MD Rifat Hossain <140188901+mohammadrifat-hossain@users.noreply.github.com>
2023-09-07 20:22:32 +0600

    Update next.config.js
    url change

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
   env: {
     DB_URL:
       "mongodb+srv://mohammadrifathossainrk:12123252@cluster0.hmrmimt.mongodb.net/databasename?retryWrites=true&w=majority",
-    PAGE_URL: "http://localhost:3000",
+    PAGE_URL: "https://simple-blog-mern.vercel.app/",
     GOOGLE_CLIENT_ID:
       "1040727717481-fvgbdknkbasjivgr9js659mfmb9jhgb5.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
-    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_URL: "https://simple-blog-mern.vercel.app/",
     NEXTAUTH_SECRET: "12123252",
   },
 };
